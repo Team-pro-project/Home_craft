2b413a78390e16e98c6b675a101576d8143fb2cc
brokenmugg <abelmajidmaamar@gmail.com>
2025-03-20 01:10:37 +0100

    home front done

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './layouts/navbar';
+import Footer from './layouts/footer';
+import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/home/Intro.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <section className="text-center py-5">
+      {/* Text Content */}
+      <p className="text-uppercase text-muted fw-bold">Furniture Store</p>
+      <h1 className="fw-bold display-4">
+        Explore the Elegance of Modern <br />
+        Home Furnishings
+      </h1>
+      <p className="text-muted fs-5">
+        Indulge in the perfect blend of style, comfort, and sophistication,
+        where innovative design seamlessly integrates with luxurious relaxation
+        in every meticulously crafted piece .
+      </p>
+
+      {/* Image */}
+      <div className="mt-4">
+        <img
+          src="https://media.gettyimages.com/id/1357529933/fr/photo/image-g%C3%A9n%C3%A9r%C3%A9e-num%C3%A9riquement-dun-salon-enti%C3%A8rement-meubl%C3%A9.jpg?s=612x612&w=0&k=20&c=j_ROCDqG937PX7ROBnwkP2l8TfJWHePNs_ADJytZVNE="
+          alt="Modern Sofa"
+          className="img-fluid rounded"
+          style={{ maxWidth: "100%", height: "auto" }}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
